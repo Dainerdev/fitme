cfeac23365fe3455afc0b794c7d0de241cca123a
Dainerdev <florianarroyo15@gmail.com>
2025-05-17 21:51:22 -0500

    Se agrega la ruta para el home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/index/header.jsx'
 import SignIn from './components/sign/signIn.jsx'
 import SignUp from './components/sign/signup.jsx'
+import Home from './components/home/home.jsx'
 import Info from './components/index/Info.jsx'
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/variables.css'
@@ -19,6 +20,7 @@ function App() {
         }/>  
         <Route path="/signin" element={<SignIn />}/>
         <Route path="/signup" element={<SignUp />}/>
+        <Route path="/home" element={<Home />}/>
       </Routes>
     </Router>
   )
