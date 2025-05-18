a9ffdb2675b4ec59eb65776fa2483cf8bc4fda99
Dainerdev <florianarroyo15@gmail.com>
2025-05-17 21:50:21 -0500

    Se crea el componente home

---
+++ src/components/home/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HeaderHome from "./headerHome";
+import Dashboard from "./dashboard";
+
+function Home() {
+  return (
+    <>
+      <HeaderHome />
+      <Dashboard />
+    </> 
+    
+  );
+}
+
+export default Home;
