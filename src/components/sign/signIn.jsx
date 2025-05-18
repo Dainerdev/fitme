1377b8444b09ca9438a510e00b56da821c6c83a8
Dainerdev <florianarroyo15@gmail.com>
2025-05-17 21:51:07 -0500

    Se implementa la ruta hacia home desde el boton "iniciar sesion"

--- src/components/sign/signIn.jsx
+++ src/components/sign/signIn.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
 import style from '../../styles/sign/signIn.module.css'
 import { useNavigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../home/home.jsx'
 
 function SignIn() {
 
@@ -11,28 +12,33 @@ function SignIn() {
     navigate('/');
   };
 
+  const home = (e) => {
+    e.preventDefault();
+    navigate('/home');
+  };
+
   return (
-    <div className={style.background}>
-      <div className={style.container}>      
-        <div className={style.content}>
-          <form className={style.loginForm}>
-            <div>              
-              <h1 className={style.info}> Iniciar <span>Sesión</span></h1>
+        <div className={style.background}>
+          <div className={style.container}>      
+            <div className={style.content}>
+              <form className={style.loginForm}>
+                <div>              
+                  <h1 className={style.info}> Iniciar <span>Sesión</span></h1>
+                </div>
+                <div className={style.inputs}>
+                  <input className={style.field} type="email" placeholder="Correo electrónico" />
+                  <input className={style.field} type="password" placeholder="Contraseña" />
+                </div>
+                <p className={style.notLog}>No estás registrado? <a href="/signup">Regístrate!</a></p>
+                <div className={style.buttonGroup}>
+                  <button className={style.logInBtn} onClick={home} type="submit">Iniciar Sesión</button>
+                  <button className={style.cancelBtn} onClick={cancel} type="submit">Cancelar</button>
+                </div>
+                <p className={style.brand}>Fit<span>.Me</span></p>
+              </form>  
             </div>
-            <div className={style.inputs}>
-              <input className={style.field} type="email" placeholder="Correo electrónico" />
-              <input className={style.field} type="password" placeholder="Contraseña" />
-            </div>
-            <p className={style.notLog}>No estás registrado? <a href="/signup">Regístrate!</a></p>
-            <div className={style.buttonGroup}>
-              <button className={style.logInBtn} type="submit">Iniciar Sesión</button>
-              <button className={style.cancelBtn} onClick={cancel} type="submit">Cancelar</button>
-            </div>
-            <p className={style.brand}>Fit<span>.Me</span></p>
-          </form>  
+          </div>
         </div>
-      </div>
-    </div>
   )
 }
 
